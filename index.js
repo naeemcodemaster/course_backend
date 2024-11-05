e789414c56d65c9d8e859a58305d2830c7d759e0
= <=>
2024-11-05 22:56:51 +0500

    set envir variables

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("This is node js running");
-console.log("this is second line");
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/',(req,res)=>{
+    res.send('Hi this is endpoint ');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running at PORT ${PORT}`);
+})
