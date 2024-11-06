bf29421c31016ef7fd2cfde8b1917b54480985c2
= <=>
2024-11-06 22:21:31 +0500

    update index.js file

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.get('/',(req,res)=>{
-    res.send('Hi this is endpoint ');
+    res.send('Hi this is endpoint updated');
+})
+
+app.post('/register',(req,res)=>{
+    res.send('hi')
 })
 
 app.listen(PORT,()=>{
