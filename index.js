63fdb4dc9dc0ae06c38eecdc12d68d2369893745
= <=>
2024-11-11 23:31:23 +0500

    add registeration api with mongodb

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const User = require('./models/User.models.js')
 
 
 dotenv.config();
 const app = express();
 
 
+connectDB();
+
+// used to accept json data
+app.use(express.json());
+
+
 const PORT = process.env.PORT || 5000;
 
 
+
 app.get('/',(req,res)=>{
-    res.send('Hi this is endpoint updated');
+    // res.send('Hi this is endpoint');
+    res.status(200).json({message:"this is root path"})
+
 })
 
-app.post('/register',(req,res)=>{
-    res.send('hi')
+
+app.post('/register',async(req,res)=>{
+    // console.log(req.body)
+    // res.status(201).json({message:"Account has been created"})
+
+    // console.log(req.body.username);
+    // const username = req.body.username;
+    // const email = req.body.email;
+    // const password = req.body.password;
+
+    // desturacturing in JS
+    const {username,email,password} = req.body;
+
+    try {
+       const userExist =  await User.findOne({email});
+
+       if(userExist){
+        return res.status(400).json({message:"User already exist"});
+       }
+
+       const user = new User({username,email,password});
+       await user.save();
+       res.status(201).json({message:"Account has been created"})
+
+
+    } catch (error) {
+        res.status(500).json({message:"Error in registration", error})
+    }
+
+
+    
+    
+
 })
+
+
+app.post('/login',(req,res)=>{
+    console.log("Login API");
+    
+})
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server running at PORT ${PORT}`);
---
+++ models/User.models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+
+const model = mongoose.model("user", Schema);
+module.exports = model;
